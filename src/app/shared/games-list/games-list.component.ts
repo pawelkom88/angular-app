0e850adc3e0ff04f4089ee70e1a16a0c35b86dca
Paw <pawel.komorkiewicz@2132.io>
2024-08-06 09:03:15 +0100

    chore: add simple logout functionality to games list component

--- src/app/shared/games-list/games-list.component.ts
+++ src/app/shared/games-list/games-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-games-list',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './games-list.component.html',
-  styleUrl: './games-list.component.css'
+  styleUrl: './games-list.component.css',
 })
 export class GamesListComponent {
+  constructor(private authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }

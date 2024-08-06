046df0839bed054b03a895e28a425bfb534121ba
Paw <pawel.komorkiewicz@2132.io>
2024-08-06 19:07:09 +0100

    chore: Remove unnecessary dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // You may need to create this file if it doesn't exist.
-import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module'; // Importing the CoreModule
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClient],
+  declarations: [],
+  imports: [BrowserModule, AppRoutingModule, CoreModule,AppComponent],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
 export class AppModule {}

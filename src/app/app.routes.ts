1bb6d60207f11ac84cab95f75adad95852049161
Paw <pawel.komorkiewicz@2132.io>
2024-08-06 09:00:15 +0100

    chore: refactor app component
    - add router outlet
    - add route path config
    - add AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,36 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/services/auth/auth-guard.guard';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { GamesListComponent } from './shared/games-list/games-list.component';
 import { LoginComponent } from './shared/login/login.component';
 import { SignupComponent } from './shared/signup/signup.component';
 
+export const RoutePathsConfig = {
+  login: 'login',
+  signup: 'signup',
+  games: 'games',
+  notFound: '**',
+} as const;
+
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'games', component: GamesListComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
+  { path: RoutePathsConfig.login, title: 'Login', component: LoginComponent },
+  {
+    path: RoutePathsConfig.signup,
+    title: 'Signup',
+    component: SignupComponent,
+  },
+  {
+    path: RoutePathsConfig.games,
+    title: 'Games',
+    component: GamesListComponent,
+    // children: [],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    title: 'Home',
+    redirectTo: RoutePathsConfig.login,
+    pathMatch: 'full',
+  },
+  { path: '**', title: 'Page not found', component: PageNotFoundComponent },
 ];

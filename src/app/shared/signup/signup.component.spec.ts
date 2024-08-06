bcde95d184bec7beedf5f21131a048e4a795e944
Paw <pawel.komorkiewicz@2132.io>
2024-08-06 19:54:07 +0100

    feat: Add signup component with basic markup and test

--- src/app/shared/signup/signup.component.spec.ts
+++ src/app/shared/signup/signup.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
+import { AuthService } from '../../core/services/auth/auth.service';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
+  let service: AuthService;
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let debugElement: DebugElement;
+  let usernameInputElement: HTMLInputElement;
+  let passwordInputElement: HTMLInputElement;
+  let signupButton: HTMLButtonElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupComponent]
-    })
-    .compileComponents();
+      imports: [SignupComponent, ReactiveFormsModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter(routes),
+      ],
+    }).compileComponents();
 
+    service = TestBed.inject(AuthService);
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
+    
+    usernameInputElement = debugElement.nativeElement.querySelector(
+      'input[name="username"]'
+    );
+    passwordInputElement = debugElement.nativeElement.querySelector(
+      'input[name="password"]'
+    );
+
+    signupButton = debugElement.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should bind the form controls to the input elements', () => {
+    usernameInputElement.value = 'test@example';
+    usernameInputElement.dispatchEvent(new Event('input'));
+    passwordInputElement.value = 'Password123';
+    passwordInputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(usernameInputElement.value).toBe('test@example');
+    expect(passwordInputElement.value).toBe('Password123');
   });
 });
--- src/app/shared/signup/signup.component.ts
+++ src/app/shared/signup/signup.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  signUpForm: FormGroup = new FormGroup({});
+  errorMessage = '';
 
+  private authSubscription: Subscription = new Subscription();
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$'
+          ),
+        ],
+      ],
+    });
+  }
+
+  createUser(){
+    
+  }
 }

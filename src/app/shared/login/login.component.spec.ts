467db322e30326c2508a75effe78ab911950200b
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-09 13:09:56 +0100

    fix: fix test for login component

--- src/app/shared/login/login.component.spec.ts
+++ src/app/shared/login/login.component.spec.ts
@@ -1,11 +1,12 @@
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { throwError } from 'rxjs';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { LoginComponent } from './login.component';
-import { provideHttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let service: AuthService;
@@ -15,11 +16,29 @@ describe('LoginComponent', () => {
   let usernameInputElement: HTMLInputElement;
   let passwordInputElement: HTMLInputElement;
   let loginButton: HTMLButtonElement;
+  let mockAuthService: jest.Mocked<AuthService>;
+  let mockActivatedRoute: Partial<ActivatedRoute>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent, ReactiveFormsModule],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn(),
+            findUser: jest.fn(),
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: jest.fn(),
+          },
+        },
+      ],
     }).compileComponents();
 
     service = TestBed.inject(AuthService);
@@ -73,14 +92,15 @@ describe('LoginComponent', () => {
 
   it('should display error message if username is invalid', () => {
     jest
-      .spyOn(service, 'findUser')
+      .spyOn(service, "findUser")
       .mockReturnValue(
         throwError(() => new Error('Invalid username or password'))
       );
     component.loginUser();
     fixture.detectChanges();
 
-    const errorMessageElement = fixture.debugElement.nativeElement.querySelector('strong');
+    const errorMessageElement =
+      fixture.debugElement.nativeElement.querySelector('strong');
     expect(errorMessageElement).toBeTruthy();
   });
 });

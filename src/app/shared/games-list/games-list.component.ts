19f8cad3087a2d6066836b85d0bf0ea0cbe7a01f
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-11 14:03:20 +0100

    chore: Implement list component

--- src/app/shared/games-list/games-list.component.ts
+++ src/app/shared/games-list/games-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../components/nav/nav.component';
+import { ListComponent } from "../components/list/list.component";
 
 @Component({
   selector: 'app-games-list',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, ListComponent],
   templateUrl: './games-list.component.html',
   styleUrl: './games-list.component.css',
 })

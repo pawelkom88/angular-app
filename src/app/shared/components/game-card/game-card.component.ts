cb6470435360ffabb58bd9b5a7009b7658bdead5
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-11 16:36:03 +0100

    feat: Add game card component and style it

--- src/app/shared/components/game-card/game-card.component.ts
+++ src/app/shared/components/game-card/game-card.component.ts
@@ -1,12 +1,14 @@
+import { Game } from '@/app/core/services/games/types';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-game-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.css',
 })
 export class GameCardComponent {
-  @Input() game: any;
+  @Input() game = {} as Game;
 }

aae986d033b1e6155d03ff49799f35e7a35a91cc
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-24 12:23:57 +0100

    feat: Add games service

--- src/app/core/services/games/games.service.ts
+++ src/app/core/services/games/games.service.ts
@@ -1,19 +1,36 @@
+import { environment } from '@/environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Game } from './types';
+import { map, Observable } from 'rxjs';
+import { Game, UserGames } from './types';
+
+// TODO: fallback if there are no games
+// loading state
+// error state
 
 @Injectable({
   providedIn: 'root',
 })
 export class GamesService {
   allGames: Game[] = [];
-  userGames: Game[] = [];
+  userGames: UserGames[] = [];
   delay: number = 1000;
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getGames() {}
+  getGames(): Observable<Game[]> {
+    return this.http.get<UserGames[]>(`${environment.apiUrl}/userGames`).pipe(
+      map((games: UserGames[]) => {
+        const allUserGames: Game[] = games.flatMap(
+          (userGames) => userGames.games
+        );
+
+        this.allGames = allUserGames;
+        return allUserGames;
+      })
+    );
+  }
 
   getGamesByUser() {}
 }

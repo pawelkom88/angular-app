1d7a34cda37fdf5eef2b3e0eb49f327e5d38793e
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-25 10:57:09 +0100

    chore: add error handling and fix getGameById method

--- src/app/core/services/games/games.service.ts
+++ src/app/core/services/games/games.service.ts
@@ -1,7 +1,7 @@
 import { environment } from '@/environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, map, Observable } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Game, UserGames } from './types';
 
 // TODO: fallback if there are no games
@@ -12,6 +12,7 @@ import { Game, UserGames } from './types';
   providedIn: 'root',
 })
 export class GamesService {
+  errorMessage: string = '';
   game$: Observable<Game | undefined> = new Observable<Game | undefined>();
   allGames$: Observable<Game[]> = new Observable<Game[]>();
   userGames$: Observable<Game[]> = new Observable<Game[]>();
@@ -21,7 +22,7 @@ export class GamesService {
 
   getGames(): Observable<Game[]> {
     return this.http.get<UserGames[]>(`${environment.apiUrl}/userGames`).pipe(
-      delay(2000),
+      delay(500),
       map((games: UserGames[]) => {
         const allUserGames: Game[] = games.flatMap(
           (userGames) => userGames.games
@@ -29,17 +30,25 @@ export class GamesService {
 
         this.userGames$ = this.allGames$;
         return allUserGames;
+      }),
+
+      catchError((error: Error) => {
+        this.errorMessage = error.message;
+        return of([]);
       })
     );
   }
 
   getGameById(gameId: number): Observable<Game | undefined> {
-    this.game$ = this.allGames$.pipe(
-      delay(2000),
-      map((allGames) => allGames.find((game) => game.id === gameId))
+    return this.getGames().pipe(
+      map((allGames) => allGames.find((game) => {
+        return game.id === gameId
+      })),
+      catchError((error: Error) => {
+        this.errorMessage = error.message;
+        return of(undefined);
+      })
     );
-
-    return this.game$;
   }
 
   getGamesByUser() {}

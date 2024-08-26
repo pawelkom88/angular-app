fdec7fd70f07e5de2e339d7d017bcbdb94724945
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-08-26 19:05:35 +0100

    fix: fix test - mock param id

--- src/app/shared/game-details/game-details.component.spec.ts
+++ src/app/shared/game-details/game-details.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 import { GameDetailsComponent } from './game-details.component';
 
 describe('GameDetailsComponent', () => {
@@ -8,9 +11,22 @@ describe('GameDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameDetailsComponent]
-    })
-    .compileComponents();
+      imports: [GameDetailsComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: jest.fn(),
+            snapshot: {
+              params: { id: 1 },
+              url: [{ path: 'test' }],
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameDetailsComponent);
     component = fixture.componentInstance;
